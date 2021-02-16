315425713a365a1b3c91aece283719ce9be7b3b1
Soyeong Jeong <starsuzi@naver.com>
2021-02-16 19:40:19 +0900

    more metric

--- 7_evaluate.sh
+++ 7_evaluate.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-INPUT_BASE_PATH="data/output/non_offensive/pegasus_xsum/"
+#INPUT_BASE_PATH="data/output/total/pegasus_reddit/"
+INPUT_BASE_PATH="data/output/non_offensive/pegasus_reddit/"
+#QREL_PATH="data/text_format/qrels_test_text_binary.txt"
 QREL_PATH="data/text_format/qrels_test_non-offensive_text_binary.txt"
 
 for RUN in ${INPUT_BASE_PATH}*.txt
@@ -12,7 +14,7 @@ do
     anserini/tools/eval/trec_eval.9.0.4/trec_eval -m map -q \
                                             ${QREL_PATH} \
                                             ${RUN} > ${RUN%.*}.results
-    anserini/tools/eval/trec_eval.9.0.4/trec_eval -m map -m recip_rank \
+    anserini/tools/eval/trec_eval.9.0.4/trec_eval -m map -m recip_rank -m P.10 -m ndcg_cut.10 \
                                             ${QREL_PATH} \
                                             ${RUN}
 done
