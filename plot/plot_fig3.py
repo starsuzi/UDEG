bc66bc907e10527139406600476c0a5a6f8b8c7e
ddehun <ddehun@kaist.ac.kr>
2021-03-18 20:37:16 +0900

    feat: add top-k different generated num into figure3

--- plot/plot_fig3.py
+++ plot/plot_fig3.py
@@ -11,8 +11,8 @@ plt.rcParams["font.family"] = "Times New Roman"
 
 MAP_QL_LINE = [0.1728, 0.2078, 0.2125, 0.2224, 0.2306, 0.2364]
 MAP_BM25_LINE = [0.2118, 0.2308, 0.2307, 0.2334, 0.2386, 0.2341]
-MAP_QL_RM3_LINE = [0.1504, 0.1762, 0.1857, 0.1997, 0.2121, 0.2163]
-MAP_BM25_RM3_LINE = [0.2121, 0.231, 0.2272, 0.2298, 0.2344, 0.235]
+MAP_QL_TOPK_LINE = [0.1728, 0.2053, 0.2152, 0.2202, 0.2201, 0.2177]
+MAP_BM25_TOPK_LINE = [0.2118, 0.2291, 0.2277, 0.2239, 0.2189, 0.2149]
 
 
 x1 = [0, 1, 2, 3, 4, 5]
@@ -20,10 +20,10 @@ plt.grid()
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 
-plt.plot(x1, MAP_BM25_LINE, "b*--", label="BM25")
-plt.plot(x1, MAP_QL_LINE, "rs--", label="QL")
-# plt.plot(x1, MAP_BM25_RM3_LINE, "g+-", label="BM25+RM3")
-# plt.plot(x1, MAP_QL_RM3_LINE, "c^-", label="QL+RM3")
+plt.plot(x1, MAP_BM25_LINE, "b*--", label="MCdrop(BM25)")
+plt.plot(x1, MAP_QL_LINE, "r^--", label="MCdrop(QL)")
+plt.plot(x1, MAP_BM25_TOPK_LINE, "go:", label="Top-K(BM25)")
+plt.plot(x1, MAP_QL_TOPK_LINE, "cs:", label="Top-K(QL)")
 
 
 # plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')    # 설명 참고
@@ -32,7 +32,7 @@ axes.set_ylim([0.16, 0.25])
 
 plt.xlabel("Number of generated sentences", fontsize=14)
 plt.ylabel("MAP", fontsize=14)
-plt.legend(loc=4, fontsize=14)
+plt.legend(loc=4, fontsize=12)
 
 plt.xticks(x1)
 
